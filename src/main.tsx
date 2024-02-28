25ed1ff8576018bf9a2a4f3baa7e3564c750b2bd
NoHaxito <santifeb2034@gmail.com>
2024-02-28 00:00:14 +0000

    Initial Commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root')
-if (!container) throw new Error('Failed to find the root element')
+import App from "./App";
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
 createRoot(container).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
